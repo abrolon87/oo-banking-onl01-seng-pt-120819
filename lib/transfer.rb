64d934a2862ea2ca9fc1cb80fe0fcc25eadc109b
Amanda Rolon <abrolon87@gmail.com>
2020-01-09 16:49:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   attr_reader :sender
-  attr_accessor :transfer_amount
+  attr_accessor :transfer_amount, :status
   def initialize(sender, receiver, status)
     @sender = sender
     @recevier = receiver
