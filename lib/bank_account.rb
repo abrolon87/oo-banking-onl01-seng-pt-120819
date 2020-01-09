7ec88c1dfef799898d7a61bf80f1a3fa4c974633
Amanda Rolon <abrolon87@gmail.com>
2020-01-09 16:41:12 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
   attr_reader :name
   attr_accessor :balance, :status
-  
+
   def initialize(name)
     @name = name
     @balance = 1000
@@ -9,7 +9,7 @@ class BankAccount
   end
 
   def deposit(amount)
-    @balance << amount
+    balance += amount
 
   end
 
