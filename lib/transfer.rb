b890ae584b1d5c5ac241ad11da42af06052179a0
Amanda Rolon <abrolon87@gmail.com>
2020-01-09 17:05:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,12 @@ class Transfer
     "Transaction rejected. Please check your account balance."
   end
 
-  def
-
+  def reverse_transfer
+  if valid? && receiver.balance > amount && self.status == "complete"
+    receiver.balance -= amount
+    sender.balance += amount
+    self.status = "reversed"
+  else
+    reject_transfer
   end
 end
